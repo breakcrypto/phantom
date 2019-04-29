5cc58eb7405b9c8a50fd04ac3545d06be8d5dd56
breakcrypto <35466877+breakcrypto@users.noreply.github.com>
2019-04-29 13:01:33 -0400

    Update conversion.go
    Filter out bad addresses returned by explorers.

--- pkg/phantom/conversion.go
+++ pkg/phantom/conversion.go
@@ -19,14 +19,17 @@ func ConvertVersionStringToInt(str string) uint32 {
 	return uint32(version)
 }
 
-func SplitAddress(pair string) wire.NetAddress {
+func SplitAddress(pair string) (wire.NetAddress, error) {
 	ipPort := strings.Split(pair, ":")
+	if len(ipPort) != 2 {
+		return wire.NetAddress{}, errors.New("invalid ip:port pair")
+	}
 	ip := ipPort[0]
 	port, _ := strconv.Atoi(ipPort[1])
 	return wire.NetAddress{time.Now(),
 		0,
 		net.ParseIP(ip),
-		uint16(port)}
+		uint16(port)}, nil
 }
 
 func SplitAddressList(bootstraps string) (addresses []wire.NetAddress) {
